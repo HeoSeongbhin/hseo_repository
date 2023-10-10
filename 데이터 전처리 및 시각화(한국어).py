0f12236fdcbc953438ece43943714eec4e9a1efd
HeoSeongbhin <hsbb1266@gmail.com>
2023-10-11 02:13:13 +0900

    Update 데이터 전처리 및 시각화(한국어).py
    데이터 전처리 및 시각화(한국어)

--- 데이터 전처리 및 시각화(한국어).py
+++ 데이터 전처리 및 시각화(한국어).py
@@ -1,49 +1,83 @@
-from soynlp.normalizer import *
+from soynlp.normalizer import repeat_normalize
 import matplotlib.pyplot as plt
+from konlpy.tag import Okt
 from wordcloud import WordCloud
 import re
+from urllib.parse import quote
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import urllib.request
 
-# 정규화
-korea_text = repeat_normalize(
-    "Python은 놀라운 프로그래밍 Python언어입니다. 데이터 Python분석, 웹 개발, 인공지능 등 Python다양한 분야에서 사용됩니다. Python은 간결하고 읽기 쉬운 코드를 작성하는 데 도움이 됩니다.",
-    num_repeats=1,
-)
+def fetch_titles(query):
+    # 기사 헤드라인 정보 요소들
+    change = quote(query, encoding="utf-8")
+    url = f"https://search.naver.com/search.naver?where=news&sm=tab_jum&query={change}"
 
-# 토큰화
-from konlpy.tag import Okt,Kkma, Komoran
-tokenizer = Okt()
-token=tokenizer.morphs(korea_text)
-print(token)
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
+    html_class = soup.find_all(class_="news_tit")
+    
+    return [tit.text.strip() for tit in html_class]
 
-# 불용어처리
-stop_words=['은','놀라운','입니다','등','다양한','에서','됩니다','은','하고','읽기','쉬운','를','하는','데','이','됩니다','.',',',',','.','.']
+def normalize_and_tokenize(titles):
+    # 정규화
+    normalized_titles = [repeat_normalize(title, num_repeats=1) for title in titles]
+    
+    # 토큰화
+    okt = Okt()
+    tokens = [okt.morphs(title) for title in normalized_titles]
+    
+    return tokens
 
-result=[]
-for word in token:
-    if word not in stop_words:
-        result.append(word)
+def remove_stopwords(tokens):
+    #파일 불러오기(한국어 불용어 리스트 txt파일)
+    stop_words_file_path = "C:\\Users\\user\\Downloads\\stopword (1).txt"  
+    with open(stop_words_file_path, "r", encoding="utf-8") as file:
+        stop_words = set(file.read().split('\n'))
 
-print(result)
-list=' '.join(result)
+    # 불용어 제외
+    filtered_tokens = [[word for word in token if word not in stop_words] for token in tokens]
+    
+    return filtered_tokens
 
-# 워드 클라우드     
-from PIL import Image
-import numpy as np
-im = Image.open('heart.png')
-mask_arr = np.array(im)
-wordcloud = WordCloud(
-    font_path="C:/Windows/Fonts/Malgun.ttf",
-    background_color="white",
-    colormap='autumn',
-    width=700,
-    height=800,
-    random_state = 43,
-    mask = mask_arr,
-    prefer_horizontal = True
-).generate(list) 
+def main():
+    #검색결과 전처리전 헤드라인들 출력 후 다른 함수들 호출 후 전처리 결과 출력
+    query = input("검색어를 입력하세요: ")
+    
+    titles = fetch_titles(query)
+    print("검색 결과:")
+    for idx, title in enumerate(titles):
+        print(f"{idx + 1}. {title}")
 
-# 이미지
-plt.figure(figsize=(5,5))
-plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("off")
-plt.show()
+    tokens = normalize_and_tokenize(titles)
+    filtered_tokens = remove_stopwords(tokens)
+
+    print("\n불용어 제외한 토큰:")
+    text_for_wordcloud = ' '.join([' '.join(token) for token in filtered_tokens])
+    print(text_for_wordcloud)
+    
+    # 워드 클라우드     
+    from PIL import Image
+    import numpy as np
+    im = Image.open('C:\\Users\\user\\Desktop\\heart.png')
+    mask_arr = np.array(im)
+    wordcloud = WordCloud(
+        font_path="C:/Windows/Fonts/Malgun.ttf",
+        background_color="white",
+        colormap='autumn',
+        width=700,
+        height=800,
+        random_state = 43,
+        mask = mask_arr,
+        prefer_horizontal = True
+    ).generate(text_for_wordcloud) 
+
+    # 이미지
+    plt.figure(figsize=(5,5))
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+
+#데이터 전처리 후 시각화 실행
+if __name__ == "__main__":
+    main()
